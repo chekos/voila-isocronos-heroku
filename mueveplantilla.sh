eff054600defd1d21005059693ed58947308c5c7
Sergio Sánchez Zavala <chekos.data@gmail.com>
2019-07-07 23:13:01 -0700

    Update mueveplantilla.sh

--- mueveplantilla.sh
+++ mueveplantilla.sh
@@ -2,10 +2,10 @@
 # cp -r ~/voila-template ../../srv/conda/envs/notebook/share/jupyter/voila/templates/default
 
 # mueve plantilla
-rm -rf ~/.local/share/jupyter/voila/templates
+rm -rf .heroku/python/share/jupyter/voila/templates
 
-cp ./voila-template/nbconvert_templates/voila.tpl ~/.local/share/jupyter/voila/templates/default/nbconvert_templates/voila.tpl
+cp ./voila-template/nbconvert_templates/voila.tpl .heroku/python/share/jupyter/voila/templates/default/nbconvert_templates/voila.tpl
 
 # agrega css
-cp ./voila-template/static/tacosdedatos-global.css ~/.local/share/jupyter/voila/templates/default/static/tacosdedatos-global.css
-cp ./voila-template/static/tacosdedatos-screen.css ~/.local/share/jupyter/voila/templates/default/static/tacosdedatos-screen.css
+cp ./voila-template/static/tacosdedatos-global.css .heroku/python/share/jupyter/voila/templates/default/static/tacosdedatos-global.css
+cp ./voila-template/static/tacosdedatos-screen.css .heroku/python/share/jupyter/voila/templates/default/static/tacosdedatos-screen.css
