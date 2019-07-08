245558f0441b8da78ff2f022332eef89015ba390
Sergio Sánchez Zavala <chekos.data@gmail.com>
2019-07-07 22:24:58 -0700

    Update mueveplantilla.sh

--- mueveplantilla.sh
+++ mueveplantilla.sh
@@ -2,9 +2,10 @@
 # cp -r ~/voila-template ../../srv/conda/envs/notebook/share/jupyter/voila/templates/default
 
 # mueve plantilla
-rm -rf ../../srv/conda/envs/notebook/share/jupyter/voila/templates/default/nbconvert_templates/voila.tpl
-cp ./voila-template/nbconvert_templates/voila.tpl ../../srv/conda/envs/notebook/share/jupyter/voila/templates/default/nbconvert_templates/voila.tpl
+rm -rf ~/.local/share/jupyter/voila/templates
+
+cp ./voila-template/nbconvert_templates/voila.tpl ~/.local/share/jupyter/voila/templates/default/nbconvert_templates/voila.tpl
 
 # agrega css
-cp ./voila-template/static/tacosdedatos-global.css ../../srv/conda/envs/notebook/share/jupyter/voila/templates/default/static/tacosdedatos-global.css
-cp ./voila-template/static/tacosdedatos-screen.css ../../srv/conda/envs/notebook/share/jupyter/voila/templates/default/static/tacosdedatos-screen.css
+cp ./voila-template/static/tacosdedatos-global.css ~/.local/share/jupyter/voila/templates/default/static/tacosdedatos-global.css
+cp ./voila-template/static/tacosdedatos-screen.css ~/.local/share/jupyter/voila/templates/default/static/tacosdedatos-screen.css
