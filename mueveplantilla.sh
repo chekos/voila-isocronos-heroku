1b417ad7df8de82b388b9853d560994826ee9c7d
Sergio Sánchez Zavala <chekos.data@gmail.com>
2019-07-08 00:23:25 -0700

    Update mueveplantilla.sh

--- mueveplantilla.sh
+++ mueveplantilla.sh
@@ -2,17 +2,17 @@
 # cp -r ~/voila-template ../../srv/conda/envs/notebook/share/jupyter/voila/templates/default
 
 # crea plantilla
-mkdir ~/.local/share/jupyter/voila/templates/tdd-template
+mkdir .heroku/python/share/jupyter/voila/templates/tdd-template
 #cd ~/.local/share/jupyter/voila/templates/tdd-template
 
 # copia contenidos
-cp -r .heroku/python/share/jupyter/voila/templates/default/nbconvert_templates ~/.local/share/jupyter/voila/templates/tdd-template/
-cp -r .heroku/python/share/jupyter/voila/templates/default/templates ~/.local/share/jupyter/voila/templates/tdd-template/
+cp -r .heroku/python/share/jupyter/voila/templates/default/nbconvert_templates .heroku/python/share/jupyter/voila/templates/tdd-template/
+cp -r .heroku/python/share/jupyter/voila/templates/default/templates .heroku/python/share/jupyter/voila/templates/tdd-template/
 
 # mueve plantilla
-rm -f ~/.local/share/jupyter/voila/templates/tdd-template/nbconvert_templates/voila.tpl
-cp ~/voila-template/nbconvert_templates/voila.tpl ~/.local/share/jupyter/voila/templates/tdd-template/nbconvert_templates/voila.tpl
+rm -f .heroku/python/share/jupyter/voila/templates/tdd-template/nbconvert_templates/voila.tpl
+cp ~/voila-template/nbconvert_templates/voila.tpl .heroku/python/share/jupyter/voila/templates/tdd-template/nbconvert_templates/voila.tpl
 
 # agrega css
-cp ~/voila-template/static/tacosdedatos-global.css ~/.local/share/jupyter/voila/templates/tdd-template/static/tacosdedatos-global.css
-cp ~/voila-template/static/tacosdedatos-screen.css ~/.local/share/jupyter/voila/templates/tdd-template/static/tacosdedatos-screen.css
+cp ~/voila-template/static/tacosdedatos-global.css .heroku/python/share/jupyter/voila/templates/tdd-template/static/tacosdedatos-global.css
+cp ~/voila-template/static/tacosdedatos-screen.css .heroku/python/share/jupyter/voila/templates/tdd-template/static/tacosdedatos-screen.css
